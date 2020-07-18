9e2a2ac7fec42fb765a9303656b6d56607a29586
nemo-omen <nemo.omen@gmail.com>
2020-07-18 11:50:09 -0500

    Added prism

--- services/posts_service.ts
+++ services/posts_service.ts
@@ -2,6 +2,8 @@ import { v4 } from "https://deno.land/std/uuid/mod.ts";
 import { moment } from "https://deno.land/x/moment/moment.ts";
 import { slugify } from "https://deno.land/x/slugify/mod.ts";
 
+const dummyJS = "```console.log('Hello, world!')```";
+
 const posts: Array<object> = [
   {
     _id: v4.generate(),
@@ -38,6 +40,23 @@ const posts: Array<object> = [
       `Let's make a full-featured blogging app without using any build tools, just  to see if we can.`,
     featuredImage: "https://picsum.photos/800/400",
     slug: "how_i_made_this_blog",
+  },
+  {
+    _id: v4.generate(),
+    createdAt: moment().format("MMMM Do YYYY, h:mm:ss a"),
+    title: "An example without a featured image",
+    content:
+      `I just want to have an example post without a featured image to work with so I can make sure the styling is good. The other thing I should make sure to do is include some code blocks, because that's certainly going to be needed in a blog about coding.
+
+<pre><code class="lang-javascript">console.log('Hello, world!')</code></pre>
+
+Okay, let's do another paragraph with some inline code. How about a nice <code class="lang-javascript">const name = 'Jeff Caldwell';</code> to see how it displays inside a paragraph.
+
+I might need to use another theme or make some customizations to this one.
+`,
+    excerpt:
+      `We're going to need a dummy example with some code blocks to make sure we're formatting that correctly.`,
+    slug: "an_example_without_a_featured_image",
   },
 ];
 
